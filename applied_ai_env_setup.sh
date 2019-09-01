41c5d7e771df8567a23fa8c7730e11835fe21d0c
parth394 <33023330+parth394@users.noreply.github.com>
2019-09-01 23:55:51 +0530

    Add files via upload

---
+++ applied_ai_env_setup.sh
@@ -0,0 +1,11 @@
+pip3 install pandas matplotlib numpy scipy scikit-learn seaborn keras Pillow plotly
+pip3 install testresources
+pip3 install launchpadlib
+pip3 install tensorflow
+pip3 install jupyter
+pip3 install https://github.com/ipython-contrib/jupyter_contrib_nbextensions/tarball/master
+pip3 install jupyter_nbextensions_configurator
+jupyter contrib nbextension install 
+jupyter nbextensions_configurator enable 
+pip3 install graphviz distance fuzzywazzy imblearn networkx spacy folium prettytable tqdm xgboost gpxpy skmultilearn surprise stellargraph tensorboard gensim
+
